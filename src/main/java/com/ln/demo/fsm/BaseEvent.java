056b6521e3a3c3f9d4b83b73084bcc752eaa1ac6
翻斗花园胡图图 <ln@bowong.ai>
2024-09-18 11:26:05 +0800

    基础工程提交

---
+++ src/main/java/com/ln/demo/fsm/BaseEvent.java
@@ -0,0 +1,8 @@
+package com.ln.demo.fsm;
+
+/**
+ * @Author : Ln
+ * @create 2024/9/18 11:12
+ */
+public interface BaseEvent {
+}
---
+++ src/main/java/com/ln/demo/fsm/BaseStatus.java
@@ -0,0 +1,9 @@
+package com.ln.demo.fsm;
+
+/**
+ * @Author : Ln
+ * @create 2024/9/18 11:10
+ */
+public interface BaseStatus {
+
+}
